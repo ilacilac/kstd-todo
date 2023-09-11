d3f1a0c60d71296d79b942eab01edc1880d252f3
ilacilac <qkralswl307@gmail.com>
2023-09-11 14:48:59 +0900

    Feat: isDone logic

--- components/Todo/TodoListItem.tsx
+++ components/Todo/TodoListItem.tsx
@@ -1,11 +1,12 @@
 import React, { Dispatch, SetStateAction, useState } from "react";
-import { Box, Button, ListItem, Typography } from "@mui/material";
+import { Box, Button, Checkbox, ListItem, Typography } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { Draggable } from "react-beautiful-dnd";
 import { Todo } from "../../types/todo";
 import ModalPortal from "components/Modal/ModalPortal";
 import TodoModal from "components/Modal/TodoModal";
+import { updateTodosToServer } from "service/todos";
 
 type TodoListItemProps = {
   todo: Todo;
@@ -45,6 +46,14 @@ const TodoListItem: React.FC<TodoListItemProps> = ({
     console.log("a");
     handleOpen();
   };
+
+  const handleCheckbox = async (id: string) => {
+    const newTodos = todos.map((_todo) =>
+      _todo.id === id ? { ..._todo, isDone: !_todo.isDone } : _todo
+    );
+    setTodosArray(newTodos);
+    updateTodosToServer(newTodos);
+  };
   return (
     <>
       <Draggable draggableId={todo.id} index={index}>
@@ -55,7 +64,13 @@ const TodoListItem: React.FC<TodoListItemProps> = ({
             {...provided.draggableProps}
             {...provided.dragHandleProps}
           >
-            <Typography>{todo.task}</Typography>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Checkbox
+                checked={todo.isDone}
+                onChange={() => handleCheckbox(todo.id)}
+              />
+              <Typography>{todo.task}</Typography>
+            </Box>
             <Box>
               <Button
                 sx={buttonCommonStyle}
